d90c02914c8c8c9c681baae7711d4a042232d8ec
Mustafa Güler <mustafaguler45567@gmail.com>
2022-06-10 03:03:51 +0300

    Grade Calculator

---
+++ NotHesaplama/NotHesaplama/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double mgn, vko, fko, vn, fn, ortalama;
+            mgn = Convert.ToDouble(textBox1.Text);
+            vko = Convert.ToDouble(textBox2.Text);
+            fko = Convert.ToDouble(textBox3.Text);
+            vn = Convert.ToDouble(textBox4.Text);
+            fn= Convert.ToDouble(textBox5.Text);
+            ortalama = (vn*vko)/100+(fn*fko)/100;
+            labelOrt.Text = ortalama.ToString();
+            if (ortalama < mgn)
+                labelSonuc.Text = "Kaldınız.";
+            else
+                labelSonuc.Text = "Geçtiniz.";
+        }
+    }
+}
